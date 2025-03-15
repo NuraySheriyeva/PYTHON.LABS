fca24f238ec90e3ec0378deeb4eaf0a0302584aa
NuraySheriyeva <n_sheriyeva@kbtu.kz>
2025-03-15 22:23:27 +0500

    Finished

--- PyGame/lab7.py/micky.py
+++ PyGame/lab7.py/micky.py
@@ -1,6 +1,8 @@
 import pygame 
 from sys import exit
 import os
+import time
+from math import sin, cos, radians
 
 pygame.init()
 #                                width,height
@@ -20,9 +22,12 @@ left_arm= pygame.image.load('../images/leftarm.png')
 left_arm = pygame.transform.scale(left_arm,(50, 500))
 
 right_arm= pygame.image.load('../images/rightarm.png')
-right_arm = pygame.transform.scale(right_arm,(700, 600))
+right_arm = pygame.transform.scale(right_arm,(350, 250))
 
-text_surface= test_font.render("Micky Clocky", False, 'White')
+text_surface= test_font.render("Micky Clocky", False, 'Black')
+
+pivot_x, pivot_y =500, 250
+hand_length = 0
 
 while True:
     for event in pygame.event.get():
@@ -30,12 +35,32 @@ while True:
             pygame.quit()
             exit()
     
+    screen.fill('White')
 
 #               our surface, position
     screen.blit(clock_surface,(250,50))
-    screen.blit(left_arm,(482,20))
-    screen.blit(right_arm,(152,-40))
-    screen.blit(text_surface, (400,12))
-                
+    screen.blit(text_surface, (380,40))
+    
+    current_s = time.localtime().tm_sec
+    angle_s = current_s * 6
+
+    current_m = time.localtime().tm_min
+    angle_m = current_m * 6
+
+    rotate_s= pygame.transform.rotate(left_arm, -angle_s)
+    rotate_m= pygame.transform.rotate(right_arm, -angle_m)
+
+    hand_rect_s=rotate_s.get_rect(center=( pivot_x + hand_length * cos(radians(angle_s - 90)),  # -90 aligns it correctly
+        pivot_y + hand_length * sin(radians(angle_s - 90))
+    )) 
+    pygame.draw.circle(screen, (0, 0, 0), (pivot_x, pivot_y), 5)  # Small dot for pivot
+    screen.blit(rotate_s, hand_rect_s.topleft)
+
+    hand_rect_m=rotate_m.get_rect(center=( pivot_x + hand_length * cos(radians(angle_m - 90)),  # -90 aligns it correctly
+        pivot_y + hand_length * sin(radians(angle_m - 90))
+    ))
+    pygame.draw.circle(screen, (0, 0, 0), (pivot_x, pivot_y), 5)  # Small dot for pivot
+    screen.blit(rotate_m, hand_rect_m.topleft)
+
     pygame.display.update()
     clock.tick(60)
