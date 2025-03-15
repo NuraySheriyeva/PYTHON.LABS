7ba90ab55f2e168d448084ec1f4f44d64740c081
NuraySheriyeva <n_sheriyeva@kbtu.kz>
2025-03-15 21:06:18 +0500

    DONE

--- PyGame/lab7.py/ball.py
+++ PyGame/lab7.py/ball.py
@@ -15,14 +15,26 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-    circle_x-=5
-    circle_y-=5
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_UP]:
+        circle_y-=20
+    if keys[pygame.K_DOWN]:
+        circle_y+=20
+    if keys[pygame.K_LEFT]:
+        circle_x-=20
+    if keys[pygame.K_RIGHT]:
+        circle_x+=20
+        
     if circle_x < 25: circle_x=25
     if circle_y < 25: circle_y=25
+    if circle_x > 775: circle_x=775
+    if circle_y > 375: circle_y=375
 
     #             R    G    B
     screen.fill((255, 255, 255))
     #                            R   G  B    position  radius
     pygame.draw.circle(screen, (255, 0, 0), (circle_x, circle_y), 25)
+    
+    
     pygame.display.update()
     clock.tick(60)
