9b0db057303ce88c5ec0a4b34faf14990e73bb00
NuraySheriyeva <n_sheriyeva@kbtu.kz>
2025-03-29 20:28:49 +0500

    new

--- PyGame/lab7.py/music.py
+++ PyGame/lab7.py/music.py
@@ -1,5 +1,6 @@
 import pygame
 from sys import exit
+import os
 
 pygame.init()
 
@@ -7,12 +8,17 @@ screen = pygame.display.set_mode((800, 400))
 clock = pygame.time.Clock()
 pygame.display.set_caption('Musicing')
 
+# Ensure Python starts from the script's directory
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
 play = pygame.image.load('../images/play.png')
-pause = pygame.image.load('../images/pause.jpg')
+pause = pygame.image.load('../images/pause.png')
 next = pygame.image.load('../images/next.png')
 prev = pygame.image.load('../images/previous.png')
 
-pygame.draw.rectangle('White', (200, 200))
+play = pygame.transform.scale(play, (50, 50))
+
+pygame.draw.rect(screen, 'White', (200, 150, 450, 100))
 
 while True:
     for event in pygame.event.get():
